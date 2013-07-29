f97dca49bfcda944bb7c05a27ba3d1f0189e2485
zagorulkinde <zagorulkinde@me.com>
2013-07-29 12:29:09 +0400

    added mail method notification

---
+++ notifier.py
@@ -0,0 +1,16 @@
+'''
+Base class for notification
+'''
+class notification(object):
+    def __init__(self, method, relist):
+        self.method = method
+        self.relist = relist
+    def sendNotification():
+        pass
+    
+class mail_notification(notification):
+    def __init__():
+        pass
+    def sendNotification():
+        pass
+
