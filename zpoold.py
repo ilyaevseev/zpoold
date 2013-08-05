d94384b2c686e660f33e0f58e5e230f8ce1c209f
zagorulkinde <zagorulkinde@me.com>
2013-08-05 13:36:12 +0400

    modified:   zpoold.py

--- zpoold.py
+++ zpoold.py
@@ -45,7 +45,20 @@ class zpoold(Daemon):
         print dm.getdiskMap(disk_names)
 
 if __name__ == '__main__':
-    z = zpoold()
-    #z.setUp()
-    z.run()
+    import sys
+    z = zpoold('/var/tmp/zpoold.pid')
+    if len(sys.argv) == 2:
+        if 'start' == sys.argv[1]:
+            z.start()
+        elif 'stop' == sys.argv[1]:
+            z.stop()
+        elif 'restart' == sys.argv[1]:
+            z.restart()
+        else:
+            print "Unknown command"
+            sys.exit(2)
+            sys.exit(0)
+    else:
+        print "usage: %s start|stop|restart" % sys.argv[0]
+        sys.exit(2)
 
