2b4db54c07e6268f795c87a617f4d1d00ff0579d
zagorulkinde <zagorulkinde@me.com>
2013-08-05 13:52:38 +0400

    modified:   zpoold.py

--- zpoold.py
+++ zpoold.py
@@ -1,21 +1,33 @@
 from cmparser import zpool_status,zpool_list_h, zpool_autoreplace, diskmap
+from notifier import mail_notification
 from daemon import Daemon
 from threading import Timer
+import time
+
+email = {'host':'',
+        'port':'',
+        'name':'',
+        'password':'',
+        'fromaddr':'',
+        'toaddr':'',
+        }
 
 class zpoold(Daemon):
     def run(self):
-        print 'Data'
+        while(True):
+           time.sleep(1000)
+           self.setUp()
 
     def setUp(self):
         zl = zpool_list_h()
-        za = zpool_autoreplace()
         zs = zpool_status()
+        mail = mail_notification(email['host'], email['port'], email['name'], email['password'])
         dm = diskmap()
         #get full list of avialable zpool names
         zname = [zname['name'] for zname in zl.get_zpool_list_h()]
         #get config
-        print zname
-        print '\n'
+        #print zname
+        #print '\n'
         #get all disk names
         zconf = []
         disk_names = []
@@ -28,21 +40,23 @@ class zpoold(Daemon):
                 for name in conf:
                     for n in name:
                         disk_names.append(n['name'])
-        print disk_names
+                        if n['state'] != 'ONLINE':
+                            mail.sendNotification(email['fromaddr'], email['toaddr'], str(conf), n['state'])
+        #print disk_names
         full_paths = []
         #get full paths of the disks
         for disk in disk_names:
             full_paths.append(dm.findPathByName(disk))
-        print full_paths 
+        #print full_paths 
         #get paths and ids:
         ids = []
         for p in [path for path in full_paths if path!='']:
             ids.append(dm.findIdBySymLinks(p))
-        print 'ids:'
-        print ids
-        print '\n'
-        print 'disk map:'
-        print dm.getdiskMap(disk_names)
+        #print 'ids:'
+        #print ids
+        #print '\n'
+        #print 'disk map:'
+        #print dm.getdiskMap(disk_names)
 
 if __name__ == '__main__':
     import sys
