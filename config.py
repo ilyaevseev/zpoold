4252cf3c758d1aa05461521742795ff4efed7e30
zagorulkinde <zagorulkinde@me.com>
2013-08-07 15:52:56 +0400

    new file:   config.py

---
+++ config.py
@@ -0,0 +1,16 @@
+config = {
+    'mail_conf':{
+        'mail_hostname':'smtp.yandex.ru',
+        'mail_port':'25',
+        'mail_login':'@yandex.ru',
+        'mail_pass':'',
+        'use_tls':'True',
+        'mail_frequency_min':'10',
+        'mail_from':'@yandex.ru',
+        'mail_to':'@gmail.com',
+        },
+    'daemon':{
+        'pid_path':'/var/tmp/zpoold.pid',
+        'daemon_freq_s':'30'
+        }
+    }
