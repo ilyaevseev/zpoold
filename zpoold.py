0f942fa14ca8b6bc8ce9c4798cf054686a3c96b6
zagorulkinde <zagorulkinde@me.com>
2013-09-15 21:30:22 +0400

    added basic logging for zpoold.py

--- zpoold.py
+++ zpoold.py
@@ -3,58 +3,64 @@ from notifier import mail_notification
 from daemon import Daemon
 import time
 from config import config
-
+import logging
 
 class zpoold(Daemon):
     def run(self):
         self.setUp()
 
     def setUp(self):
+        logging.basicConfig(filename='log.txt', level=logging.DEBUG)
         zl = zpool_list_h()
         zs = zpool_status()
         dm = diskmap()
         self.id_unv = ''
 
-        #get full list of avialable zpool names
         zname = [zname['name'] for zname in zl.get_zpool_list_h()]
-        #get config
-        #print zname
-        #print '\n'
-        #get all disk names
+        logging.debug('get full list of avialable zpool names')
+        logging.debug(zname)
         zconf = []
         disk_names = []
         for name in zname:
             zconf.append(zs.get_zpool_status(name))
+            logging.debug('zpool status')
+            logging.debug(zs.get_zpool_status(name))
             for z in zconf:
                 conf = z['config']
+                logging.debug('pool config')
+                logging.debug(conf)
                 for name in conf:
+                    logging.debug('name')
+                    logging.debug(name)
                     for n in name:
                         disk_names.append(n['name'])
+                        logging.debug('disk names')
+                        logging.debug(disk_names)
                         if n['state'] == 'UNAVAIL':
                             mail = mail_notification()
                             mail.sendNotification(message = str(disk_names), subj=n['state'])
                             oldpath = dm.findIdBySymLinks((dm.findPathByName(n['name'])))['by-path']
+                            logging.debug('old path:')
+                            logging.debug(oldpath)
                             if self.id_unv == '':
                                 self.id_unv = dm.findIdBySymLinks((dm.findPathByName(n['name'])))['by-id']
+                                logging.debug('id unv:')
+                                logging.debug(self.id_unv)
                             elif self.id_unv != dm.findIdBySymLinks((dm.findPathByName(n['name'])))['by-id']:
-                                print 'Startinf replacing:'
+                                logging.debug('Startinf replacing:')
                                 zs.replaceDisk(zname, oldpath)
-
-        #print disk_names
         full_paths = []
-        #get full paths of the disks
+        logging.debug('get full paths of the disks')
         for disk in disk_names:
             full_paths.append(dm.findPathByName(disk))
-        #print full_paths
-        #get paths and ids:
+        logging.debug(full_paths)
         ids = []
         for p in [path for path in full_paths if path!='']:
             ids.append(dm.findIdBySymLinks(p))
-        print 'ids:'
-        print ids
-        print '\n'
-        print 'disk map:'
-        print dm.getdiskMap(disk_names)
+        logging.debug('ids:')
+        logging.debug(ids)
+        logging.debug('disk map:')
+        logging.debug(dm.getdiskMap(disk_names))
 
 if __name__ == '__main__':
     import sys
